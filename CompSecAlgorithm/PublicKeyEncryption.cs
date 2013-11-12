6a89f034d89a722749603153eb71483eecc2af69
Matt Ryder <matt@mattryder.co.uk>
2013-11-12 14:53:45 +0000

    initial commit

---
+++ CompSecAlgorithm/PublicKeyEncryption.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompSecAlgorithm
+{
+    class PublicKeyEncryption
+    {
+        private string _password;
+
+        public string PrivateKey
+        {
+            get;
+            set;
+        }
+
+        public string PublicKey
+        {
+            get;
+            set;
+        }
+
+        public PublicKeyEncryption(string password = null)
+        {
+            _password = password;
+        }
+
+        public void Encrypt() 
+        {
+
+        }
+
+
+    }
+}
